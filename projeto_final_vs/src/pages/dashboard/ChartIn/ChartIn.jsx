824eeca68eca5f105c0a50747f9bf531c083970c
glasser95 <glasserpaulo95@hotmail.com>
2022-12-09 13:02:24 -0300

    Projeto finalizado

--- projeto_final_vs/src/pages/dashboard/ChartIn/ChartIn.jsx
+++ projeto_final_vs/src/pages/dashboard/ChartIn/ChartIn.jsx
@@ -1,11 +1,91 @@
 import React, { Component } from 'react'
+import Chart from 'react-apexcharts'
+import data from '../../../data/bankFull.json'
+import { ColorMap } from '../../../components/contexts/Color/ColorMap'
+
+const campaigns = []
+
+data.forEach(currentValue => {
+  if(!campaigns.includes(currentValue.campaign)){
+    campaigns.push(currentValue.campaign)
+  }
+})
+
+const campaigns2 = campaigns.slice(0, 5)
+console.log("Campaigns: ", campaigns)
+
+const campaignsDist = []
+
+campaigns2.forEach( currentValue => {
+
+  campaignsDist.push (data.filter(item => item.campaign === currentValue).length)
+}
+)
+
 
 export default class ChartIn extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        title: {
+          text: 'Quntidade de contactos por campanha'
+        },
+        chart: {
+          type: 'bar',
+          height: 350,
+          
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 4,
+            horizontal: false,
+            distributed: true
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+       
+        xaxis: {
+          categories: [...campaigns2]
+        },
+
+        fill:{
+          colors: [...ColorMap]
+        },
+
+        legend: {
+          show: false
+        },
+      },
+
+      series: [
+        {
+          name: "Quantidade",
+          data: [...campaignsDist]
+        }
+      ]
+    };
+  }
+
   render() {
+    
     return (
-      <div className='baseChart' style={{width: '450px'}}>
-        Chart
+      <div>
+        <div className="row">
+          <div className="mixed-chart baseChart background_blur">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="500"
+            />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
